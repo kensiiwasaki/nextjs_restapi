6d24c1a86c2e3543e73f15c36316a0e4531a11fc
けんし <kensiiwasaki@gmail.com>
2022-06-19 03:20:52 +0900

    added cors ehite list

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:3000",
+    "http://localhost:3000", "https://nextjs-blog-beta-ebon-42.vercel.app"
 ]
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
